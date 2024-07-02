a12ba6af029cbf89a3831dd57d9ba3f50b0fda48
André <andre.soares@altxtech.net>
2024-07-02 17:24:54 -0300

    Implement model validation to pass tests

--- airbyte-integrations/connectors/source-base-forkeap/source_base_forkeap/model/contact.py
+++ airbyte-integrations/connectors/source-base-forkeap/source_base_forkeap/model/contact.py
@@ -1,9 +1,11 @@
+from os import kill
 from .base import Base
 from .exceptions import ValidationError
 from dataclasses import dataclass
 from typing import List, Optional, Any
 from datetime import datetime, date, timezone
 import re
+import pycountry
 
 '''
 Note to self:
@@ -26,39 +28,72 @@ class Address(Base):
     zip_four: Optional[str] = None
 
     def validate(self):
-        # If region is specified, country_code is required
-        if self.region and not self.country_code:
-            raise ValidationError("Country code is required when region is specified", "contry_code")
+        
+        valid_fields = ["BILLING", "SHIPPING", "OTHER"]
+        if self.field not in valid_fields:
+            raise ValidationError(f"Invalid field: {self.field}", "field")
+        
+        # Validate country and region
+        country = pycountry.countries.get(alpha_3=self.country_code)
+        if not country:
+            raise ValidationError(f"Invalid country code: f{self.country_code}", "country_code")
+
+        if self.region_code:
+            region = pycountry.subdivisions.get(code=self.region_code)
+
+            if not region:
+                raise ValidationError(f"Invalid region code: {self.region_code}", "region_code")
+
+            if region.country != country:
+                raise ValidationError(f"Region {self.region_code} is not a subdivision of {self.country_code}", "region_code")
+
 @dataclass
 class Company(Base):
-    id: str
+
+    # Decided to make id optional because we could potentially send just the company name and have the Keap destination figure out the id
+    # downstream
+
+    id: Optional[str]
     company_name: Optional[str] = None
 
     def validate(self):
-        if not self.id:
-            raise ValidationError("Company ID is required", "id")
+
+        if not re.match(r"\d+", self.id):
+            raise ValidationError("id should include only digits", "id") 
 
 @dataclass
 class FaxNumber(Base):
     field: str
     number: str
-    type: Optional[str]
+    type: Optional[str] = None
 
     def validate(self):
 
-        if self.field not in ["FAX1", "FAX2", "FAX_NUMBER_FIELD_UNSPECIFIED"]:
+        if self.field not in ["FAX1", "FAX2"]:
             raise ValidationError("Invalid fax number field", "field")
 
-        if not re.match(r"\+?[0-9]+", self.number):
+        if not re.match(r"^(\+?\d{1,3}[\s-])?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}(x\d{1,5})?$", self.number):
             raise ValidationError("Invalid fax number", "number")
+        
+        if self.type:
+            valid_types = ["Work", "Home", "Other"]
+            if self.type not in valid_types:
+                raise ValidationError(f"Valid types are {', '.join(valid_types)}", "type")
 
 @dataclass
 class CustomFieldValue(Base):
+
+    # Differently from Company, custom field id should be required because:
+    # 1 - Keap API requires it 
+    # 2 - Would be 'hacky' for the Keap destination to figure out the id based on content only
+
     id: str
     content: Any 
 
     def validate(self):
-        pass
+        
+        if not re.match(r"\d+", self.id):
+            raise ValidationError("'id' should include only digits", "id")
 
 @dataclass
 class EmailAddress(Base):
@@ -91,29 +126,42 @@ class OriginRequest(Base):
 
 @dataclass
 class PhoneNumber(Base):
-    extension: Optional[str] = None
-    field: Optional[str] = None
-    number: Optional[str] = None
+    field: str
+    number: str
     type: Optional[str] = None
+    extension: Optional[str] = None
     
     def validate(self):
 
-        if self.number:
-            if not re.match(r"\+?[0-9]+", self.number):
-                raise ValidationError("Invalid phone number", "number")
-
-        if self.field not in ["PHONE_NUMBER_FIELD_UNSPECIFIED", "PHONE1", "PHONE2", "PHONE3", "PHONE4", "PHONE5"]:
+        if self.field not in ["PHONE1", "PHONE2", "PHONE3", "PHONE4", "PHONE5"]:
             raise ValidationError("Invalid phone number field", "field")
 
+        if not re.match(r"^(\+?\d{1,3}[\s-])?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}(x\d{1,5})?$", self.number):
+            raise ValidationError("Invalid phone number", "number")
+        
+        if self.type:
+            valid_types = ["Work", "Home", "Mobile", "Other"]
+            if self.type not in valid_types:
+                raise ValidationError(f"Valid types are {', '.join(valid_types)}", "type")
+
+        if self.extension:
+            if not re.match(r"\d{1,5}", self.extension):
+                raise ValidationError(f"Extension should be 1 to 5 digits", "extension")
+
+
 @dataclass
-class SocialAccount:
+class SocialAccount(Base):
 
     name: str
     type: str
 
     def validate(self):
-        if self.type not in ["SOCIAL_ACCOUNT_TYPE_UNSPECIFIED", "FACEBOOK", "LINKED_IN", "TWITTER", "INSTAGRAM", "SNAPCHAT", "YOUTUBE", "PINTEREST"]:
+        if self.type not in ["FACEBOOK", "LINKED_IN", "TWITTER", "INSTAGRAM", "SNAPCHAT", "YOUTUBE", "PINTEREST"]:
             raise ValidationError("Invalid social account type", "type")
+
+        url_re = r"https?:\/\/\w+(\.\w+)+"
+        if not re.match(url_re, self.name):
+            raise ValidationError(f"{self.name} is not a valid url", "name")
 
 @dataclass
 class UtmParameter:
@@ -176,3 +224,49 @@ class Contact(Base):
             if email.field in fields:
                 raise ValidationError(f"Email field '{email.field}' is repeated", "email_addresses")
             fields[email.field] = True
+
+        # Phone numbers fiels should be not be repeated
+        if self.phone_numbers:
+            fields = {}
+            for number in self.phone_numbers:
+                if number.field in fields:
+                    raise ValidationError(f"Phone field '{number.field}' is repeated", "phone_numbers")
+                fields[number.field] = True
+
+        if self.fax_numbers:
+            fields = {}
+            for number in self.fax_numbers:
+                if number.field in fields:
+                    raise ValidationError(f"Fax field '{number.field}' is repeated", "fax_numbers")
+                fields[number.field] = True
+
+        if self.social_accounts:
+            fields = {}
+            for acc in self.social_accounts:
+                if acc.type in fields:
+                    raise ValidationError(f"Fax field '{acc.type}' is repeated", "social_accounts")
+                fields[acc.type] = True
+
+        # Prefixes
+        if self.prefix:
+            valid_prefixes = ['Mr.', 'Mrs.', 'Ms.', "Dr."]
+            if self.prefix not in valid_prefixes:
+                raise ValidationError(f"Valid prefixes are {', '.join(valid_prefixes)}", "prefix")
+
+        # Suffixes
+        if self.suffix:
+            valid_suffixes = ["Jr", "PhD", "I", "II", "III", "IV", "V"]
+            if self.suffix not in valid_suffixes:
+                raise ValidationError(f"Valid suffixes are {', '.join(valid_suffixes)}", "suffix")
+        
+        if self.contact_type:
+            valid_contact_types = ["Lead", "Customer", "Other"]
+            if self.contact_type not in valid_contact_types:
+                raise ValidationError(f"Valid types are {', '.join(valid_contact_types)}", "contact_type")
+
+        # Website
+        if self.website:
+            url_re = r"https?:\/\/\w+(\.\w+)+"
+            if not re.match(url_re, self.website):
+                raise ValidationError(f"{self.website} is not a valid url", "website")
+
