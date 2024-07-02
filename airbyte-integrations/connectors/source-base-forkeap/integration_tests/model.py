243e962b8cecc43324675e0eb31503aaadc857cc
André <andre.soares@altxtech.net>
2024-07-02 19:30:29 -0300

    Sucessfull 250 records integrationt test

--- airbyte-integrations/connectors/source-base-forkeap/integration_tests/model.py
+++ airbyte-integrations/connectors/source-base-forkeap/integration_tests/model.py
@@ -16,15 +16,14 @@ KEAP_ACCESS_TOKEN = os.environ["KEAP_ACCESS_TOKEN"]
 
 def test_contact_intensive():
 
-    for _ in range(1):
+    for i in range(250):
+        print("Test number ", i)
         _test_contact()
 
 def _test_contact():
 
     # Generate a fake contact
     contact = gen.fake_contact()
-    print(contact)
-    
     # Try to create it against the Keap API
     _create_contact(contact)
 
@@ -32,14 +31,14 @@ def _test_contact():
 def _create_contact(contact: Contact):
 
     payload = json.dumps(contact.to_dict(), indent=4, default=str)
-    print(payload)
     url = "https://api.infusionsoft.com/crm/rest/v2/contacts"
     head = {"Authorization": "Bearer " + KEAP_ACCESS_TOKEN, "Content-Type": "application/json"}
 
     response = requests.post(url, headers = head, data = payload)
 
     if not response.ok:
-        # Save to file for later debugging
-        with open("secrets/failed_contact.json", "w") as f:
+        print("Failed payload: ")
+        print(payload)
+        with open("secrets/failed_payload.json", "w") as f:
             f.write(payload)
         raise Exception(response.json()["message"])
